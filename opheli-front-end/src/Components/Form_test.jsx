46f10b6ecbf1debb9f0dc1014a227262bbaa9b87
CodenameWaddles <clovis.oudart@wanadoo.fr>
2022-06-17 16:52:16 +0200

    added browser storage example

--- opheli-front-end/src/Components/Form_test.jsx
+++ opheli-front-end/src/Components/Form_test.jsx
@@ -1,10 +1,14 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import form from "./Form";
 import $ from "jquery";
 
 function App() {
     const [name, setName] = useState("");
     const [result, setResult] = useState("");
+
+    useEffect(() => { //execute au lancement de la page
+        setResult(sessionStorage.getItem("info")); //chercher la donnée en stockage
+    })
 
     const handleChange = (e) => {
         setName(e.target.value);
@@ -19,6 +23,7 @@ function App() {
             data: form.serialize(),
             success(data) {
                 setResult(data);
+                sessionStorage.setItem("info",data); //foutre la donnée en stockage du navigateur
             },
         });
     };
