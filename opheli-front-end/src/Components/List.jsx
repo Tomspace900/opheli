d2b3414393cc989d0a1cdd59dacf12b0b6a841dc
MathPays <57558201+MathPays@users.noreply.github.com>
2022-06-20 15:34:33 +0200

    ajout de la liste

--- opheli-front-end/src/Components/List.jsx
+++ opheli-front-end/src/Components/List.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import '../CSS/List.css';
+import '../CSS/Login.css';
 
 const List = () => {
+    $.ajax({
+        type: "POST",
+        url: form.attr("action"),
+        data: form.serialize(),
+        success(data) {
+            setResult(data);
+            sessionStorage.setItem("info",data); //foutre la donnée en stockage du navigateur
+        },
+    });
+
     return (
-        <div className="page">
-            <table>
-                <tr>
-                    <td>Ordonnance</td>
-                    <td>Description</td>
-                </tr>
-                <tr>
-                    <td>Ordonnance</td>
-                    <td>Description</td>
-                </tr>
-            </table>
+        <div>
+            <div className="login">
+                <span className="login-title">Liste des ordonnances</span>
+            </div>
+            <div className="page">
+                <table>
+                    <tr>
+                        <td>Ordonnance</td>
+                        <td>Date</td>
+                        <td>Description</td>
+                        <td>Modifier</td>
+                        <td>Supprimer</td>
+                    </tr>
+                    <tr>
+                        <td>Ordonnance</td>
+                        <td>Date</td>
+                        <td>Description</td>
+                        <td>Modifier</td>
+                        <td>Supprimer</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 };
