1e2a439316c735b2a6208f8012fcf365c81eaeb8
MathPays <57558201+MathPays@users.noreply.github.com>
2022-06-24 21:05:03 +0200

    test

--- opheli-back-end/PHP/login/auth_test.php
+++
@@ -1,5 +0,0 @@
-<?php
-include '../utiles/connexion.php';
-header('Access-Control-Allow-Origin: http://localhost:3000');
-$user = $_POST['name'];
-$pass = $_POST['pass'];
--- opheli-front-end/src/Components/UpdateOrdo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UpdateOrdo = () => {
-    return <div></div>;
-};
-
-export default UpdateOrdo;
