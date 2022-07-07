53120649352b04f854baca8865e94ccf2fc15778
CodenameWaddles <clovis.oudart@wanadoo.fr>
2022-07-07 21:43:45 +0200

    redirection post création d'ordonnance

--- opheli-front-end/src/Components/CreateOrdo.jsx
+++ opheli-front-end/src/Components/CreateOrdo.jsx
@@ -16,7 +16,7 @@ function App({ idMedecin, role }) {
     const navigate = useNavigate();
 
     console.log(role);
-    //if (role  != 'medecin') {navigate('/Error')}
+    if (role  != 'medecin') {navigate('/Error')}
 
     const handleIdPatient = (e) => {
         setIdPatient(e.target.value);
@@ -70,7 +70,7 @@ function App({ idMedecin, role }) {
                     nbRenouvTotal: nbUseSimple,
                     soinsSimples: soinsSimples,
                     notes: notes,
-                });
+                }).then(navigate('/'));
                 break;
 
             default:
@@ -83,10 +83,7 @@ function App({ idMedecin, role }) {
                         nbRenouvTotal: nbUseSimple,
                         soinsSimples: soinsSimples,
                         notes: notes,
-                    })
-                    .then((r) => {
-                        console.log(r);
-                    });
+                    }).then(navigate('/'));
                 break;
         }
     }
